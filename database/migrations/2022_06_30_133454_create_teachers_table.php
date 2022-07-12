a983bb93335a838d4946466e880a57c0afa42e57
snoguchi <snoguchi.nbsr@gmail.com>
2022-07-12 14:22:58 +0900

    student_teacherテーブル作成

--- database/migrations/2022_06_30_133454_create_teachers_table.php
+++ database/migrations/2022_06_30_133454_create_teachers_table.php
@@ -23,6 +23,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->timestamp('deleted_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/seeders/StudentTeacherSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Student;
+use App\Models\Teacher;
+use Illuminate\Support\Facades\DB;
+
+
+class StudentTeacherSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 0; $i < 10; $i++){
+
+            // studentsとteachersテーブルのidカラムをランダムに並び替え、先頭の値を取得
+            $set_student_id = Student::select('id')->inRandomOrder()->first()->id;
+
+            // $set_teacher_id = Teacher::select('id')->inRandomOrder()->first()->id;
+            $set_teacher_id = Teacher::select('id')->inRandomOrder()->first()->id;
+
+            // クエリビルダを利用し、上記のモデルから取得した値が、現在までの複合主キーと重複するかを確認
+            $student_teacher = DB::table('student_teacher')
+                            ->where([
+                                ['student_id', '=', $set_student_id],
+                                ['teacher_id', '=', $set_teacher_id]
+                            ])->get();
+
+            // 上記のクエリビルダで取得したコレクションが空の場合、外部キーに上記のモデルから取得した値をセット
+            if($student_teacher->isEmpty()){
+                DB::table('student_teacher')->insert(
+                    [
+                        'student_id' => $set_student_id,
+                        'teacher_id' => $set_teacher_id,
+                        'subject_id' => rand(1, 10),
+                    ]
+                );
+            }else{
+                $i--;
+            }
+        }
+    }
+}
