a8d716bf5f7f91c835dcd567742a88b16a7d5316
snoguchi <snoguchi.nbsr@gmail.com>
2022-07-15 14:09:55 +0900

    fakerをrequireに(修正3)

--- database/seeders/StudentTeacherSeeder.php
+++ database/seeders/StudentTeacherSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Student;
 use App\Models\Teacher;
+use App\Models\Subject;
 use Illuminate\Support\Facades\DB;
 
 
